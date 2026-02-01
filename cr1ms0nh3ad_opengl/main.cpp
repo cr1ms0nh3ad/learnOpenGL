f6a8105cb444b0cd437921d7898386e5d124be84
cr1ms0nh3ad <bigsmoke31453@gmail.com>
2026-02-01 17:16:10 +0500

    урааа прямоугольниккккк

--- cr1ms0nh3ad_opengl/main.cpp
+++ cr1ms0nh3ad_opengl/main.cpp
@@ -7,8 +7,29 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // settings
+// --------
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 1024;
+
+// vertex shader source code
+// -------------------------
+const char* vertexShaderSource =
+	"#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n" 
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+// fragment shader source code
+// ---------------------------
+const char* fragmentShaderSource =
+	"#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
+	"}\0";
 
 int main()
 {
@@ -39,6 +60,107 @@ int main()
 		return -1;
 	}
 
+	// vertex shader: compilation
+	// --------------------------
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// vertex shader: compilation status logging
+	// -----------------------------------------
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// fragment shader: compilation
+	// ----------------------------
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// shader program (linking v and f shaders together)
+	// -------------------------------------------------
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// shader program: linking status logging
+	// --------------------------------------
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING::FAILED\n" << infoLog << std::endl;
+	}
+
+	// since we've linked shader objects into the program object
+	// we no longer need them anymore
+	// ---------------------------------------------------------
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	// set up vertex data (and buffers) and configure vertex attributes
+	// ----------------------------------------------------------------
+	float vertices[] =
+	{
+		0.5f, 0.5f, 0.0f,	// top right
+		0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f,	// top left
+	};
+
+	unsigned int indices[] =
+	{
+		0, 1, 3,	// first triangle
+		1, 2, 3		// second trinagle
+	};
+
+	// copy our vertices array in a buffer for OpenGL to use
+	// -----------------------------------------------------
+	unsigned int VBO, VAO, EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// set the vertex attributes pointers
+	// ----------------------------------
+	glVertexAttribPointer
+	(
+		0,						// which vertex attribute we want to configure (location = 0)
+		3,						// size of the vertex attribute (vec3)
+		GL_FLOAT,				// a vecX in GLSL consists of floating point values
+		GL_FALSE,				// do we want the data to be normalized?
+		3 * sizeof(float),		// stride (the space between consecutive vertex attributes)
+		(void*)0				// offseit of where the position data begins in the buffer (weird ass cast)
+	);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// draw in wireframe mode
+	// ----------------------
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 	// MAIN RENDER LOOP
 	// ----------------
 	while (!glfwWindowShouldClose(window))
@@ -49,10 +171,18 @@ int main()
 
 		// render
 		// ------
-		glClearColor(1.0f, 0.325f, 0.7f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// draw
+		// ----
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 		// GLFW: swap buffer and poll IO events (keys pressed/released, mouse moved etc.)
+		// ------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
